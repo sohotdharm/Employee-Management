cec921cb533f7084faf14fbd08d21d306320c8b1
sohotdharm <dharmendu3427@gmail.com>
2021-09-21 21:33:06 +0530

    first commit

---
+++ src/Employee/conn.java
@@ -0,0 +1,19 @@
+
+package Employee;
+
+import java.sql.*;
+
+public class conn {
+    public Connection c;
+    public Statement s;
+ 
+    public conn(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///company","root","1234");
+            s = c.createStatement();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
